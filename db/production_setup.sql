06172f45708cedfc4ab1f477b8af387141996e76
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-10-18 14:27:02 +0100

    postgresql setup

---
+++ db/production_setup.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE carte_de_bucate_production;
+CREATE DATABASE carte_de_bucate_production_cache;
+CREATE DATABASE carte_de_bucate_production_queue;
+CREATE DATABASE carte_de_bucate_production_cable;
