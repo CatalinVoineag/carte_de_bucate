11c02a27156afb5bb2818400866185d2c32fa4dc
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-11-16 13:16:56 +0000

    wip

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import { initAll } from "govuk-frontend";
 import "@hotwired/turbo-rails"
 
 import "controllers"
+
+initAll();
